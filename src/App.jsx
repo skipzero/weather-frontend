d441f4802bcb443c4c6ac25a2f6a163310f07750
Skip McBride <skip@zerosquadron.com>
2024-04-30 02:13:14 -0700

    working on the chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import React, { createRef, useEffect, RefObject, useState } from 'react';
-import * as d3 from 'd3';
+import { useState } from 'react'
 import './App.scss';
-import barChartComponent from './components/stackedBarChart';
-
-
-
+import BarChartComponent from './components/stackedBarChart';
 
 const App = () => {
-  const [ data, setData ] = useState()
-  const url = 'https://zerosquadron.com'
-
-  const ref = createRef();
+  // const [ data, setData ] = useState([ 22, 56, 25, 57, 98, 32, 25, 12, 16, 76 ])
 
   return (
     <>
-      <BarChartComponent data={ data } />
+      <div>
+        <BarChartComponent />
+      </div>
+      {/* <button onClick={ () => setData([ 23, 67, 86, 21, 77, 32, 11, 87, 45, 21 ]) } >Change Data</button >
+      <button onClick={ () => setData(data.filter(item => item < 34)) } >Change Data</button > */}
     </>
   )
 
--- src/components/stackedBarChart.js
+++ src/components/stackedBarChart.js
@@ -1,91 +1,124 @@
+import React, { useRef, useEffect, useState } from 'react';
 import * as d3 from 'd3';
-
+// import './stackedBarChart.css';
 
 const BarChartComponent = () => {
-  const margin = {
-    top: 0,
-    right: 0,
-    bottom: 10,
-    left: 9
-  }
+  const [ data, setData ] = useState([])
+  const url = 'https://zerosquadron.com'
+  const ref = useRef();
 
-  const width = 928;
-  const height = 500
+  useEffect(() => {
 
-  const y01z = d3.stack()
-    .keys(d3.range(n))
-    (d3.transpose(yz))
-    .map((data, i) => data.map(([ y0, y1 ]) => [ y0, y1, i ]))
+    fetch(url)
+      .then(resp => resp.json())
+      .then(resp => resp.reduce((acc, temp, inx) => {
+        if (inx % 5 === 0)
+        {
+          if (temp.tempf)
+          {
 
-  const yMax = d3.max(yz, y => d3.max(y))
-  const y1Max = d3.max(y01z, y => d3.max(y, d => d[ 1 ]))
+            acc.push({ tempf: temp.tempf, createdAt: temp.createdAt })
+          }
+        }
+        return acc
+      }, []))
+      .then(resp => setData(resp))
+      .catch(err => console.error(err))
 
-  const x = d3.scaleBand()
-    .domain(xyz)
-    .rangeRound([ margin.left, width - margin.right ])
-    .padding(0.08)
+    if (data)
+    {
+      const margin = { top: 50, right: 30, bottom: 30, left: 60 },
+        width = 920 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+      // append the svg object to the body of the page
+      const svg = d3.select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style('backgournd', '#d3d3d3')
+        .append("g")
+        .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")")
 
-  const y = d3.scaleLinear()
-    .donmain([ 0, y1Max ])
-    .range([ height = margin.bottom, margin.top ])
+      var x = d3.scaleLinear()
+        .domain([ 0, data.length - 1 ])
+        .range([ 0, width ]);
+      svg.append("g")
+        .attr("class", "myXaxis")   // Note that here we give a class to the X axis, to be able to call it later and modify it
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+      // .attr("opacity", "0")
 
-  const color = d3.scaleSequential(d3.interpolate)
-    .domain([ -0, 5 * n, 1.5 * n ]);
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([ 0, 500000 ])
+        .range([ height, 0 ]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
 
-  const svg = d3.create('svg')
-    .attr('viewBox', [ 0, 0, width, height ])
-    .attr('width', width)
-    .attr('height', height)
-    .attr('style', 'max-width: 100%; height: auto;')
+      // Add dots
+      svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.createdAt); })
+        .attr("cy", function (d) { return y(d.tempf); })
+        .attr("r", 1.5)
+        .style("fill", "#69b3a2")
 
-  const rect = svg.selectAll('g')
-    .data(y01z)
-    .join('g')
-    .attr('fill', (d, i) => color(i))
-    .selectAll('rect')
-    .attr('x', (d, i) => x(i))
-    .attr('y', height = margin.bottom)
-    .attr('width', x.bandwidth())
-    .attr('height', 0)
+      // new X axis
+      x.domain([ 0, 4000 ])
+      svg.select(".myXaxis")
+        .transition()
+        .duration(2000)
+        .attr("opacity", "1")
+        .call(d3.axisBottom(x));
 
-  svg.append('g')
-    .attr('transform', `translate(0, ${height - margin.bottom})`)
-    .call(d3.axisBottom(x).tickSizeOuter(0)).tickFormat(() => '')
+      svg.selectAll("circle")
+        .transition()
+        .delay(function (d, i) { return (i * 3) })
+        .duration(2000)
+        .attr("cx", function (d) { return x(d.GrLivArea); })
+        .attr("cy", function (d) { return y(d.SalePrice); })
+      console.log('BANG!!')
+      console.log(data)
+      svg.select(ref.current)
+      svg.selectAll('circle')
+        .data(data)
+        .join(
+          enter => enter.append('circle')
+            .attr('r', value => value)
+            .attr('cx', d => d * 2)
+            .attr('cy', d => d * .25),
 
-  function transitionGrouped() {
-    y.domain([ 0, yMax ]);
+          update => update.attr('class', 'updated'),
+          exit => exit.remove()
+        )
+    }
+  }, [])
 
-    rect.transition()
-      .duration(500)
-      .delay((d, i) => i * 20)
-      .attr('x', (d, i) => x(i) + x.bandwidth() / n * d[ 2 ])
-      .attr('width', x.bandwidth() / n)
-      .transition()
-      .attr('y', d => y(d[ 1 ] - d[ 9 ]))
-      .attr('height', d => y(0) - y(d[ 1 ] - d[ 0 ]))
-  }
+  return (
+    <svg ref={ ref }></svg>
+  )
 
-  function transitionStacked() {
-    y.domain([ 0, y1Max ])
-
-    rect.transition()
-      .duration(500)
-      .delay((d, i) => i * 20)
-      .attr('y', d => y(d[ 1 ]))
-      .attr('width', x.bandwidth())
-      .transition()
-      .attr('x', (d, i) => x(i))
-      .attr('width', x.bandwidth())
-
-  }
-
-  function update(layout) {
-    layout === 'stacked' ? transitionStacked() : transitionGrouped()
-  }
-
-  return Object.assign(svg.node(), {
-    update
-  })
 }
-
 export default BarChartComponent;
+
+
+// export default App;
+
+
+
+
+
+
+
+
+// function update(layout) {
+//   layout === 'stacked' ? transitionStacked() : transitionGrouped()
+// }
+
+// return Object.assign(svg.node(), {
+//   update
+// })
+// }
